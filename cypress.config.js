f14cbe3894637ae3568da92ba2ad3cbf9b0b18e2
M4deN <alexdesaran@outlook.com>
2024-11-20 10:55:23 -0300

    Config Final

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
-const { defineConfig } = require("cypress");
-const cypressSplit = require('cypress-split')
+const { defineConfig } = require('cypress');
+const cypressSplit = require('cypress-split');
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      cypressSplit(on, config)
+    setupNodeEvents (on, config) {
+      cypressSplit(on, config);
       // IMPORTANT: return the config object
-      return config
-    },
-  },
+      return config;
+    }
+  }
 });
